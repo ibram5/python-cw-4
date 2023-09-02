0883ad81cf421ed5c8879edff28107d39e79b749
ibram5 <143288555+ibram5@users.noreply.github.com>
2023-09-02 19:30:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ my_name("ibram")
 
 def my_meal(food,drink):
     print(f"I like to eat {food} and while drinking {drink}")
-    
+my_meal("kfc","pepsi")
+
 def cube(number):
-    return "the number to the power of 3"
-
-cube(input("enter the number"))
-
-
-
+    return number*number*number
 
 def by_three(number):
     if number % 3 == 0:
-        return
+      print(cube(number))
     else : 
         print(False)
+by_three(int(input("enter the number :")))
