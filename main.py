ed80596d70a89351c791b8566914f4439fb12e0e
ibram5 <143288555+ibram5@users.noreply.github.com>
2023-09-02 19:12:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def my_name(name):
+    print(f"my name is {name}")
+my_name("ibram")
+
+def my_meal(food,drink):
+    print(f"I like to eat {food} and while drinking {drink}")
+    
+def cube(number):
+    return "the number to the power of 3"
+
+cube(input("enter the number"))
+
+
+
+
+def by_three(number):
+    if number % 3 == 0:
+        return
+    else : 
+        print(False)
